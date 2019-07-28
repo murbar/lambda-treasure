446f18a41b643dcc5cf2d6b92a0f9951cf975397
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-07-28 12:52:59 -0400

    Style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Styles = styled.header`
+  text-align: center;
+  h1 {
+    margin: 2rem 0;
+    line-height: 1;
+  }
+`;
+
 export default function Header() {
   return (
-    <header>
+    <Styles>
       <h1>Lambda Treasure Hunt</h1>
-    </header>
+    </Styles>
   );
 }
