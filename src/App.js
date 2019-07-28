3abe684888fb08d3dd980d4f1047a52c8feeb8aa
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-07-28 12:53:50 -0400

    Add state, debug hotkey

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import useLocalStorageState from 'hooks/useLocalStorageState';
 import Header from 'components/Header';
+import Controls from 'components/Controls';
 import Footer from 'components/Footer';
+import useHotKeys from 'hooks/useHotkeys';
 
 function App() {
+  const [gameState, setGameState] = useLocalStorageState('gameState', {
+    current_room_id: {
+      id: null,
+      title: '',
+      description: '',
+      exits: [],
+      messages: [],
+      errors: []
+    },
+    cooldown: null,
+    cooldownStart: null,
+    mapData: null,
+    apiKey: null
+  });
+
+  useEffect(() => {}, []);
+
+  useHotKeys({
+    d: () => console.log(gameState)
+  });
+
   return (
     <div>
       <Header />
+      <Controls gameState={gameState} />
       <Footer />
     </div>
   );
