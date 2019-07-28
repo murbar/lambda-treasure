29cf01656605ca19c7c1c1c0799c91b295c022cc
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-07-28 18:01:00 -0400

    Add useCallback, rename file

---
+++ src/hooks/useGameService.js
@@ -0,0 +1,53 @@
+import { useState, useEffect, useCallback } from 'react';
+import gameService from 'services/gameService';
+import httpService from 'services/httpService';
+
+const useGameService = apiKey => {
+  const [roomData, setRoomData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [apiError, setApiError] = useState(null);
+
+  const requestWrapper = cb => async (...args) => {
+    setIsLoading(true);
+    setApiError(null);
+    try {
+      await cb(...args);
+      setIsLoading(false);
+    } catch (error) {
+      console.dir(error);
+      setApiError(error.response.data);
+      setIsLoading(false);
+    }
+  };
+
+  const checkIn = useCallback(
+    requestWrapper(async () => {
+      const { data } = await gameService.checkIn();
+      setRoomData(data);
+    }),
+    []
+  );
+
+  const move = useCallback(
+    requestWrapper(async direction => {
+      const { data } = await gameService.move(direction);
+      setRoomData(data);
+    }),
+    []
+  );
+
+  useEffect(() => {
+    httpService.setAuthKey(apiKey);
+    if (apiKey) checkIn();
+  }, [apiKey, checkIn]);
+
+  return {
+    roomData,
+    isLoading,
+    apiError,
+    checkIn,
+    move
+  };
+};
+
+export default useGameService;
