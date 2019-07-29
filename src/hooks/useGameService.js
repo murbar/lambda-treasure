809102012692d3147337d739e76170d10bbfa0b0
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-07-29 18:45:53 -0400

    Refactor file

--- src/hooks/useGameService.js
+++ src/hooks/useGameService.js
@@ -2,8 +2,40 @@ import { useState, useEffect, useCallback } from 'react';
 import gameService from 'services/gameService';
 import httpService from 'services/httpService';
 
+const parseCoordinates = coords => {
+  return coords.slice(1, coords.length - 1).split(',');
+};
+
+const initGameData = {
+  room: {
+    id: null,
+    title: '',
+    description: '',
+    players: [],
+    items: [],
+    exits: [],
+    coordinates: { x: null, y: null },
+    terrain: '',
+    elevation: 0
+  },
+  player: {
+    name: '',
+    encumbrance: 0,
+    strength: 0,
+    speed: 0,
+    gold: 0,
+    inventory: [],
+    status: []
+  },
+  cooldown: 0,
+  errors: [],
+  messages: []
+};
+
 const useGameService = apiKey => {
-  const [roomData, setRoomData] = useState(null);
+  // const [roomData, setRoomData] = useState(null);
+  // const [playerData, setPLayerData] = useState(null);
+  const [gameData, setGameData] = useState(initGameData);
   const [isLoading, setIsLoading] = useState(false);
   const [apiError, setApiError] = useState(null);
 
@@ -23,15 +55,118 @@ const useGameService = apiKey => {
   const checkIn = useCallback(
     requestWrapper(async () => {
       const { data } = await gameService.checkIn();
-      setRoomData(data);
+      const {
+        cooldown,
+        errors,
+        messages,
+        room_id,
+        title,
+        description,
+        elevation,
+        terrain,
+        players,
+        items,
+        exits
+      } = data;
+      const [x, y] = parseCoordinates(data.coordinates);
+      setGameData(prev => {
+        return {
+          ...prev,
+          cooldown,
+          errors,
+          messages,
+          room: {
+            ...prev.room,
+            id: room_id,
+            title,
+            description,
+            coordinates: { x, y },
+            elevation,
+            terrain,
+            players,
+            items,
+            exits
+          }
+        };
+      });
+    }),
+    []
+  );
+
+  const checkPlayerStatus = useCallback(
+    requestWrapper(async () => {
+      const { data } = await gameService.checkStatus();
+      const {
+        cooldown,
+        errors,
+        messages,
+        name,
+        encumbrance,
+        strength,
+        speed,
+        gold,
+        inventory,
+        status
+      } = data;
+      setGameData(prev => {
+        return {
+          ...prev,
+          cooldown,
+          errors,
+          messages,
+          player: {
+            ...prev.player,
+            name,
+            encumbrance,
+            strength,
+            speed,
+            gold,
+            inventory,
+            status
+          }
+        };
+      });
     }),
     []
   );
 
   const move = useCallback(
-    requestWrapper(async direction => {
+    requestWrapper(async (direction, nextRoomId = null) => {
       const { data } = await gameService.move(direction);
-      setRoomData(data);
+      const {
+        cooldown,
+        errors,
+        messages,
+        room_id,
+        title,
+        description,
+        elevation,
+        terrain,
+        players,
+        items,
+        exits
+      } = data;
+      const [x, y] = parseCoordinates(data.coordinates);
+      setGameData(prev => {
+        return {
+          ...prev,
+          cooldown,
+          errors,
+          messages,
+          room: {
+            ...prev.room,
+            id: room_id,
+            title,
+            description,
+            coordinates: { x, y },
+            elevation,
+            terrain,
+            players,
+            items,
+            exits
+          }
+        };
+      });
     }),
     []
   );
@@ -60,11 +195,14 @@ const useGameService = apiKey => {
   }, [apiKey, checkIn]);
 
   return {
-    roomData,
+    gameData,
     isLoading,
     apiError,
-    checkIn,
-    move
+    actions: {
+      checkPlayerStatus,
+      checkIn,
+      move
+    }
   };
 };
 
