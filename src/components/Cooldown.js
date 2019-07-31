f61d57c9666f25ccf050de594f91a62598cd9ff6
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-07-31 17:52:33 -0400

    Ensure displays on top layer

--- src/components/Cooldown.js
+++ src/components/Cooldown.js
@@ -10,10 +10,11 @@ const Styles = styled.div`
   pointer-events: none;
   width: 100%;
   text-align: center;
-  text-shadow: 0 0 1rem black, 0 0 1px black, 0 0 10rem white;
+  text-shadow: 0 0 1rem white, 0 0 1px white, 0 0 10rem white;
   span {
     font-size: 0.7em;
   }
+  z-index: 1000;
 `;
 
 export default function Cooldown({ secs }) {
