350d6b188d7d1b529dbb0bb13e6ea7e514cd71f9
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-07-31 00:21:21 -0400

    Extract Cooldown

---
+++ src/components/Cooldown.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+  position: absolute;
+  bottom: 25%;
+  font-family: ${p => p.theme.headingFont};
+  font-size: 6rem;
+  color: coral;
+  pointer-events: none;
+  width: 100%;
+  text-align: center;
+  text-shadow: 0 0 1rem black, 0 0 1px black;
+  span {
+    font-size: 0.7em;
+  }
+`;
+
+export default function Cooldown({ secs }) {
+  return secs > 0 ? (
+    <Styles>
+      Cooldown {secs}
+      <span>s</span>
+    </Styles>
+  ) : null;
+}
