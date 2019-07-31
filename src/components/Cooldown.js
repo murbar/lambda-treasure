3be4103ff63fe95c5487e35290de6424f2706474
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-07-31 13:01:29 -0400

    Update styles

--- src/components/Cooldown.js
+++ src/components/Cooldown.js
@@ -6,11 +6,11 @@ const Styles = styled.div`
   bottom: 25%;
   font-family: ${p => p.theme.headingFont};
   font-size: 6rem;
-  color: coral;
+  color: orange;
   pointer-events: none;
   width: 100%;
   text-align: center;
-  text-shadow: 0 0 1rem black, 0 0 1px black;
+  text-shadow: 0 0 1rem black, 0 0 1px black, 0 0 10rem white;
   span {
     font-size: 0.7em;
   }
