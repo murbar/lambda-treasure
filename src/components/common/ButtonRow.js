305c355fc6c1da32f5f67316f62e44692b5b52ae
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-07-31 19:08:40 -0400

    Add margin

--- src/components/common/ButtonRow.js
+++ src/components/common/ButtonRow.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Styles = styled.div``;
+const Styles = styled.div`
+  margin-top: 2rem;
+`;
 
 export default function ButtonRow({ children }) {
-  return <div>{children}</div>;
+  return <Styles>{children}</Styles>;
 }
