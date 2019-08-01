5b526740bb812c5dd331febc8f841b4701972dee
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-07-31 22:39:46 -0400

    Tweak size

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -4,7 +4,7 @@ export default styled.button`
   font-family: ${p => p.theme.font};
   border: none;
   border-radius: ${p => p.theme.inputBorderRadius};
-  font-size: 1.25em;
+  font-size: 1.1em;
   padding: 0.25em 0.75em;
   background: ${p => p.theme.colors.primary};
   color: white;
