e209bf9322367138db476aaef8c5cac11d009a34
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-08-01 10:18:33 -0400

    Style exits and players

--- src/components/RoomStats.js
+++ src/components/RoomStats.js
@@ -5,9 +5,11 @@ import OverlayBox from 'components/common/OverlayBox';
 
 const Styles = styled.div`
   line-height: 1.3;
-  font-size: 2rem;
-  max-width: 45rem;
+  max-width: 35rem;
   pointer-events: auto;
+  h3 {
+    margin: 0.5rem 0 0;
+  }
   .room {
     font-family: ${p => p.theme.headingFont};
     font-size: 1.75em;
@@ -19,13 +21,31 @@ const Styles = styled.div`
   .desc {
     font-style: italic;
   }
+  .exits {
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
+    span {
+      text-align: center;
+      text-transform: uppercase;
+      font-weight: bold;
+      display: inline-block;
+      width: 1.3em;
+      border: 1px solid black;
+      border-radius: 0.25em;
+      margin-right: 0.25em;
+      &:last-child {
+        margin-right: 0;
+      }
+    }
+  }
   .players {
     > div {
       display: flex;
       flex-wrap: wrap;
     }
     span {
-      padding: 0.25em;
+      padding: 0.25em 0.5em;
       font-size: 0.8em;
     }
   }
@@ -44,9 +64,6 @@ const Styles = styled.div`
     }
   }
   .messages {
-    h3 {
-      margin: 0.5rem 0 0;
-    }
   }
   z-index: 1500;
 `;
@@ -68,7 +85,11 @@ export default function RoomStats({ gameState, takeItem }) {
         <div className="room">#{roomId}</div>
         <div className="title">{title}</div>
         <div className="desc">{description}</div>
-        <div className="exits">Exits: {exits.map(e => e.toUpperCase()).join(', ')}</div>
+        <div className="exits">
+          {exits.map(e => (
+            <span key={e}>{e}</span>
+          ))}
+        </div>
         {!!items.length && (
           <div className="items">
             {items.map((item, i) => (
@@ -87,7 +108,7 @@ export default function RoomStats({ gameState, takeItem }) {
         )}
         {players.length > 0 && (
           <div className="players">
-            Players:
+            <h3>Players</h3>
             <div>
               {players.map((m, i) => (
                 <span key={i}>{m}</span>
