af387dcd2aebc6c54faaf20a764f1245e3c7bb5d
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-08-01 23:33:12 -0400

    Squash nasty bugs

--- src/helpers.js
+++ src/helpers.js
@@ -4,24 +4,26 @@ const getOppositeOrdinal = o => {
   const opposites = { n: 's', s: 'n', e: 'w', w: 'e' };
   return opposites[o];
 };
-
-export const updateMapData = (mapData, serverResponse, lastRoomId, thisRoomId, move) => {
+export const updateMapData = (mapData = {}, serverResponse, lastRoomId, thisRoomId, move) => {
+  console.log(mapData, serverResponse, lastRoomId, thisRoomId, move);
   if (thisRoomId in mapData) {
     // update last room with move
     if (lastRoomId !== thisRoomId) {
-      if (lastRoomId) {
+      // zero is valid
+      if (lastRoomId || lastRoomId === 0) {
         mapData[lastRoomId].exits[move] = thisRoomId;
         mapData[thisRoomId].exits[getOppositeOrdinal(move)] = lastRoomId;
       }
     }
     return { ...mapData };
-  } else {
+  } else if (thisRoomId !== null) {
     // add new room record
     const { exits } = serverResponse;
     const exitsMap = {};
     for (const e of exits) {
       exitsMap[e] = '?';
     }
+
     const room = {
       ...initRoomData,
       room_id: serverResponse.room_id,
@@ -32,14 +34,15 @@ export const updateMapData = (mapData, serverResponse, lastRoomId, thisRoomId, m
       terrain: serverResponse.terrain,
       elevation: serverResponse.elevation
     };
-    console.log(exits, exitsMap, room);
+
     mapData[thisRoomId] = room;
     if (lastRoomId !== thisRoomId) {
-      if (lastRoomId) {
+      if (lastRoomId || lastRoomId === 0) {
         mapData[lastRoomId].exits[move] = thisRoomId;
         mapData[thisRoomId].exits[getOppositeOrdinal(move)] = lastRoomId;
       }
     }
+
     return { ...mapData };
   }
 };
